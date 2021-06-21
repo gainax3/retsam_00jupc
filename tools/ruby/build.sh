eeda1e66403b6821c61b346572857c2cf50168c2
anon <anon@4chan.org>
2021-06-21 11:36:48 -0400

    Add ruby-1.8.7-p374 and build scripts

---
+++ tools/ruby/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+export MAKEFLAGS="-j$(nproc)"
+export CFLAGS="-march=native -O2 -pipe -w"
+
+cwd="$PWD"
+
+rm -rf ruby-1.8.7-p374
+tar xf ruby-1.8.7-p374.tar.bz2
+cd ruby-1.8.7-p374
+patch -p0 < ../patch/fix-cygwin-longjmp.patch
+rm -rf ext/openssl ext/tk ext/curses
+./configure --bindir="$cwd" --libdir="$cwd/lib" --with-static-linked-ext
+make
+./miniruby instruby.rb --extout=.ext -i bin-arch -i lib -i ext-comm
+cd ..
+rm -rf ruby-1.8.7-p374
