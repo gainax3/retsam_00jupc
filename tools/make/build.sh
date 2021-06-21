9e1d60249d7af2fa55e4e88639d0ec97f9697d0a
anon <anon@4chan.org>
2021-06-21 11:36:22 -0400

    Add patches and buildscript for make

---
+++ tools/make/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+export MAKEFLAGS="-j$(nproc)"
+export CFLAGS="-march=native -O2 -pipe -w"
+
+cwd="$PWD"
+
+rm -rf make-3.80-1
+tar xf make-3.80-1-src.tar.bz2
+cd make-3.80-1
+patch -p0 < ../patch/eval-conditional.patch
+patch -p0 < ../patch/eval-crash.diff
+patch -p0 < ../patch/fix-strnicmp.patch
+patch -p0 < ../patch/fix-alloca.patch
+./configure --bindir="$cwd" --disable-nls
+make
+make install-exec
+cd ..
+rm -rf make-3.80-1
