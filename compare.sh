2539724a7965531519635c0e124f133574d56875
anon <anon@4chan.org>
2021-06-25 11:03:59 -0400

    Include US Platinum's binary files, stub out building JP files.

--- compare.sh
+++ compare.sh
@@ -5,10 +5,10 @@ LENGTH=$((0x210000))
 START_OFFSET=$((0x4000))
 echo $LENGTH
 
-#hexdump -C -n $LENGTH -s $START_OFFSET diff/pokeplatinum.jp.nds > diff/baserom_platinum_j.txt # can create an empty baserom.txt if no baserom.gba
-#hexdump -C -n $LENGTH -s $START_OFFSET bin/ARM9-TS/Rom/main.srl > diff/main.txt
-hexdump -C diff/pokeplatinum.jp.nds > diff/baserom_platinum_j.txt # can create an empty baserom.txt if no baserom.gba
-hexdump -C bin/ARM9-TS/Rom/main.srl > diff/main.txt
+hexdump -C -n $LENGTH -s $START_OFFSET diff/pokeplatinum.us.nds > diff/baserom_platinum_u.txt # can create an empty baserom.txt if no baserom.gba
+hexdump -C -n $LENGTH -s $START_OFFSET bin/ARM9-TS/Rom/main.srl > diff/main.txt
+#hexdump -C diff/pokeplatinum.us.nds > diff/baserom_platinum_u.txt # can create an empty baserom.txt if no baserom.gba
+#hexdump -C bin/ARM9-TS/Rom/main.srl > diff/main.txt
 
-diff -u diff/baserom_platinum_j.txt diff/main.txt > diff/compare_dump.dump
+diff -u diff/baserom_platinum_u.txt diff/main.txt > diff/compare_dump.dump
 #split -l 10000 compare_dump.dump compare_dump_out.dump
