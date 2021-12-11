6068f9020015c3a33c548d78d1c899cd0274be24
PikalaxALT <pikalaxalt@gmail.com>
2021-12-11 15:58:33 -0500

    Decompile ScratchWriteMsg_ov111_21D2424

--- src/application/scratch/scratch.c
+++ src/application/scratch/scratch.c
@@ -2772,60 +2772,15 @@ static u8 ScratchWriteMsg( SCRATCH_WORK* wk, GF_BGL_BMPWIN* win, int msg_id, u32
 								GF_PRINTCOLOR_MAKE(f_col,s_col,b_col), NULL );
 }
 
-// NONMATCHING
-asm static u8 ScratchWriteMsg_ov111_21D2424( SCRATCH_WORK* wk, GF_BGL_BMPWIN* win, int msg_id, u32 x, u32 y, u32 wait, u8 f_col, u8 s_col, u8 b_col, u8 font )
+static u8 ScratchWriteMsg_ov111_21D2424( SCRATCH_WORK* wk, GF_BGL_BMPWIN* win, int msg_id, u32 x, u32 y, u32 wait, u8 f_col, u8 s_col, u8 b_col, u8 font )
 {
-	push {r3, r4, r5, r6, r7, lr}
-	sub sp, #0x10
-	add r4, r1, #0
-	add r1, sp, #0x38
-	ldrb r1, [r1]
-	add r5, r0, #0
-	add r0, r4, #0
-	add r7, r2, #0
-	add r6, r3, #0
-	bl GF_BGL_BmpWinDataFill
-	ldr r0, [r5, #0x38]
-	ldr r2, [r5, #0x44]
-	add r1, r7, #0
-	bl MSGMAN_GetString
-	ldr r0, [r5, #0x3c]
-	ldr r1, [r5, #0x40]
-	ldr r2, [r5, #0x44]
-	bl WORDSET_ExpandStr
-	add r0, sp, #0x3c
-	ldrb r0, [r0]
-	ldr r1, [r5, #0x40]
-	mov r2, #0
-	bl FontProc_GetPrintStrWidth
-	add r0, r0, #1
-	lsr r0, r0, #1
-	sub r3, r6, r0
-	ldr r0, [sp, #0x28]
-	add r2, sp, #0x18
-	str r0, [sp]
-	ldr r0, [sp, #0x2c]
-	str r0, [sp, #4]
-	add r0, sp, #0x38
-	ldrb r1, [r0]
-	ldrb r0, [r2, #0x18]
-	ldrb r2, [r2, #0x1c]
-	lsl r0, r0, #0x18
-	lsl r2, r2, #0x18
-	lsr r0, r0, #8
-	lsr r2, r2, #0x10
-	orr r0, r2
-	orr r0, r1
-	str r0, [sp, #8]
-	mov r0, #0
-	str r0, [sp, #0xc]
-	add r1, sp, #0x3c
-	ldrb r1, [r1]
-	ldr r2, [r5, #0x40]
-	add r0, r4, #0
-	bl GF_STR_PrintColor
-	add sp, #0x10
-	pop {r3, r4, r5, r6, r7, pc}
+    GF_BGL_BmpWinDataFill(win, b_col);
+    MSGMAN_GetString(wk->msgman, msg_id, wk->tmp_buf);
+    WORDSET_ExpandStr(wk->wordset, wk->msg_buf, wk->tmp_buf);
+    x -= (FontProc_GetPrintStrWidth(font, wk->msg_buf, 0) + 1) / 2;
+    return GF_STR_PrintColor( win, font, wk->msg_buf,
+                              x, y, wait,
+                              GF_PRINTCOLOR_MAKE(f_col,s_col,b_col), NULL );
 }
 
 //--------------------------------------------------------------
